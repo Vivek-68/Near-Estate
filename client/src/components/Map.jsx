c0242b3016ab26116302d04801caf0f2ffaed3de
Vivek-68 <vivektitan3@gmail.com>
2024-05-18 21:07:25 +0530

    fix: leaflet map css

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import Pin from './Pin';
+import { listData } from '../lib/dummydata';
 
 
 
 
 
-const Map = () => {
+const Map = ({items}) => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className='w-[100%] h-[100%]' >
+    <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className='w-[100%] h-[100%]' >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+    {
+      listData.map(item => <Pin {...item} key={item.id}/>)
+    }
   </MapContainer>
   )
 }
---
+++ client/src/components/Pin.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Marker,Popup } from 'react-leaflet'
+import { Link } from 'react-router-dom'
+
+const Pin = ({latitude,longitude,title,bedroom,price,img,id}) => {
+  return (
+    <Marker position={[latitude, longitude]}>
+      <Popup>
+        <div className='flex pr-8 gap-1'>
+        <img src={img} alt="Could not load image" className='w-[64px] h-[48px] object-cover' />
+        <div className='flex flex-col '>
+        <Link to={`/${id}`}>{title}</Link>
+        <b>{bedroom} Bedrooms</b>
+        <b>${price}</b>
+        </div>
+        </div>
+      </Popup>
+    </Marker>
+  )
+}
+
+export default Pin
